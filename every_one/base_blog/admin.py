6cdd9449da95f51c60f0e07702070508dbd8e746
Ruslan <88377783+Dealer327@users.noreply.github.com>
2023-08-22 12:11:08 +0300

    add model profile and add forms for model

--- every_one/base_blog/admin.py
+++ every_one/base_blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Profile
+from .forms import ProfileForm
 
 # Register your models here.
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('id', 'external_id', 'name')
+    form = ProfileForm
--- every_one/base_blog/models.py
+++ every_one/base_blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Profile(models.Model):
+    external_id = models.PositiveIntegerField(
+        verbose_name='ID пользователя',
+    )
+    name = models.TextField(
+        verbose_name='Имя пользователя',
+    )
+
+    def __str__(self):
+        return f'#{self.external_id} {self.name}'
+
+    class Meta:
+        verbose_name = 'Профиль'
+        verbose_name_plural = 'Профили'
