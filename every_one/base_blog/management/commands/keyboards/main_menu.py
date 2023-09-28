eb693e791f2b952b6df75db1e4f85f3308c426fb
Ruslan <88377783+Dealer327@users.noreply.github.com>
2023-09-28 23:09:31 +0300

    добавление инлаин кнопок в гл. меню

--- every_one/base_blog/management/commands/keyboards/main_menu.py
+++ every_one/base_blog/management/commands/keyboards/main_menu.py
@@ -1,7 +1,8 @@
 from aiogram import Bot
 from aiogram.types import BotCommand
-
-from commands.lexicon.lexicon_ru import LEXICON_COMMANDS
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+from commands.lexicon.lexicon_ru import Lexicon_ru, LEXICON_COMMANDS
 
 
 async def set_main_menu(bot: Bot):
@@ -10,3 +11,22 @@ async def set_main_menu(bot: Bot):
         description=description
     ) for command, description in LEXICON_COMMANDS.items()]
     await bot.set_my_commands(main_menu_commands)
+
+
+def create_inline_kb(width: int, *args: str, ) -> InlineKeyboardMarkup:
+    # Инициализируем билдер
+    kb_builder = InlineKeyboardBuilder()
+    # Инициализируем список для кнопок
+    buttons: list[InlineKeyboardButton] = []
+
+    # Заполняем список кнопками из аргументов args и kwargs
+    if args:
+        for button in args:
+            buttons.append(InlineKeyboardButton(
+                text=Lexicon_ru[button] if button in Lexicon_ru else button,
+                callback_data=button))
+    # Распаковываем список с кнопками в билдер методом row c параметром width
+    kb_builder.row(*buttons, width=width)
+
+    # Возвращаем объект инлайн-клавиатуры
+    return kb_builder.as_markup()
