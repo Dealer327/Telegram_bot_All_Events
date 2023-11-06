d72458f32f58237f072621afc19f678b866e165c
Ruslan <88377783+Dealer327@users.noreply.github.com>
2023-11-06 18:01:24 +0300

    регистрация понель эвентов в админку

--- every_one/base_blog/admin.py
+++ every_one/base_blog/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Profile, Event
 from .forms import ProfileForm
+
 
 # Register your models here.
 
@@ -9,3 +10,8 @@ from .forms import ProfileForm
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ('id', 'external_id', 'name', 'time_create', 'time_update', 'choice_month')
     form = ProfileForm
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('id', 'external_id', 'name_event', 'info_event', 'start_time', 'user_create', 'create_time')
