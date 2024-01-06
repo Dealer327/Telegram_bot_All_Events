852fa7da8ec54f2470a942c3b64a8c00fb374c73
Ruslan <88377783+Dealer327@users.noreply.github.com>
2024-01-06 16:04:35 +0300

    комментарии и постинг в канал добавленных эвентов

--- every_one/base_blog/management/commands/handlers/admin_hundlers.py
+++ every_one/base_blog/management/commands/handlers/admin_hundlers.py
@@ -1,13 +1,14 @@
 from aiogram import Router, F, Bot
 from aiogram.types import CallbackQuery, Message
 
-from ..lexicon.lexicon_ru import Lexicon_ru
-from ..keyboards.main_menu import create_inline_kb
-from ..date_base import datebase
+from base_blog.management.commands.lexicon.lexicon_ru import Lexicon_ru
+from base_blog.management.commands.keyboards.main_menu import create_inline_kb
+from base_blog.management.commands.date_base import datebase
 
 router: Router = Router()
 
 
+# Хендлер для регистрации новых эвентов, уведомление приходит админам бота
 @router.callback_query(F.data == 'Yes_reg_new_event' or F.data == 'No_reg_new_event')
 async def process_add_id_event(callback: CallbackQuery):
     if callback.data == 'Yes_reg_new_event':
@@ -15,17 +16,27 @@ async def process_add_id_event(callback: CallbackQuery):
 
         @router.message()
         async def add_id_event(message: Message, bot: Bot):
-            await datebase.add_new_event(int(message.text))
+            # получаем id эвента от админа
+            id_event = int(message.text)
+            # Публикуем эвент по id в чат бота
+            await datebase.add_new_event(id_event)
             await bot.delete_message(chat_id=message.chat.id, message_id=message.message_id)
-            await bot.delete_message(chat_id=callback.message.chat.id, message_id=callback.message.message_id)
-            all_not_read_events = await datebase.show_count_not_read_event_in_menu(message.from_user.id)
-            keyboard_menu = create_inline_kb(1, all_not_read_events, 'calendar', 'new_event')
+            # Берем этот эвент и пересылаем его в канал телеграма
+            event = await datebase.process_posting_event_in_chanel(id_event)
+            await bot.send_message(chat_id=-1002007237265, text=f'<b>{event.name_event}</b>\n\n'
+                                                                f'{Lexicon_ru["about_event"]}\n'
+                                                                f'{event.info_event}\n'
+                                                                f'{event.start_time.strftime("%Y-%m-%d в %H:%M")}')
+            # Цикл для отправки уведомление всем пользователям, что появилось новое событие в чат боте
             send_msg_users = await datebase.all_users()
             for user in send_msg_users:
+                all_not_read_events = await datebase.show_count_not_read_event_in_menu(user.external_id)
+                keyboard_menu = create_inline_kb(1, all_not_read_events, 'calendar', 'new_event')
                 try:
                     await bot.send_message(chat_id=user.external_id,
                                            text=Lexicon_ru['/start'],
                                            reply_markup=keyboard_menu
                                            )
+                # Исключение на случай если пользователь по каким то причинам нее получил уведомление
                 except Exception as e:
                     print(f"Failed to send message to user {user.name}: {e}")
