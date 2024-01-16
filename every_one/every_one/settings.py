76dd18696e5c472f871a0b5cef34c3263c45a7d7
Ruslan <88377783+Dealer327@users.noreply.github.com>
2024-01-16 16:38:11 +0300

    сервер

--- every_one/every_one/settings.py
+++ every_one/every_one/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'postgres', '0.0.0.0', '::1']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
